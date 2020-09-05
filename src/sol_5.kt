2c2afe1d5d7950300fd556090066938f62729434
Abdulqader <alhemyariabdulqader@gmail.com>
2020-09-05 14:23:36 +0300

    sol of 5TH challenge

---
+++ src/sol_5.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    var smm= Summation(1,5)
+
+    println("result is : " +(smm.sum1+smm.sum2))
+}
+
+class Summation(var1:Int,var2:Int)
+{
+    var sum1:Int=var1
+    var sum2:Int=var2
+
+
+
+
+}
